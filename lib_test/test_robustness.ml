99ac33bd0a6d0bc655011dea1caa6c76222e3baa
David Sheets <sheets@alum.mit.edu>
2013-08-03 00:10:06 +0100

    Align local server and test port

--- lib_test/test_robustness.ml
+++ lib_test/test_robustness.ml
@@ -11,7 +11,7 @@ let no_q_pkt = Dns.Packet.({
 })
 
 let go () =
-  Dns_resolver.create ~config:(`Static (["127.0.0.1",53],[])) ()
+  Dns_resolver.create ~config:(`Static (["127.0.0.1",5354],[])) ()
   >>= fun resolver ->
   Dns_resolver.send_pkt resolver no_q_pkt
   >|= Mldig_lib.print_answers
